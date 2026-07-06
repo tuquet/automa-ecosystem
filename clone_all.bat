d2696b0882be6d5f5c719ed5ceafc51d8c84f206
TuQuet <tunyk.93@gmail.com>
2026-07-06 18:02:38 +0700

    feat: run pnpm install automatically in clone scripts and devcontainer

--- clone_all.bat
+++ clone_all.bat
@@ -4,8 +4,13 @@ REM Script to clone all Automa Ecosystem repositories on Windows
 echo Cloning Automa Ecosystem repositories...
 
 gh repo clone tuquet/automa automa
+cd automa && pnpm install --ignore-scripts && cd ..
+
 gh repo clone tuquet/automa-be automa-be
+cd automa-be && pnpm install --ignore-scripts && cd ..
+
 gh repo clone tuquet/automa-cli automa-cli
+cd automa-cli && pnpm install --ignore-scripts && cd ..
 
 echo All repositories cloned successfully!
 pause
--- clone_all.sh
+++ clone_all.sh
@@ -4,7 +4,12 @@
 echo "Cloning Automa Ecosystem repositories..."
 
 gh repo clone tuquet/automa automa
+cd automa && pnpm install --ignore-scripts && cd ..
+
 gh repo clone tuquet/automa-be automa-be
+cd automa-be && pnpm install --ignore-scripts && cd ..
+
 gh repo clone tuquet/automa-cli automa-cli
+cd automa-cli && pnpm install --ignore-scripts && cd ..
 
 echo "All repositories cloned successfully!"
