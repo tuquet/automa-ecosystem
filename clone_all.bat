1527d72e3c575235607bb369a803f079517141ce
TuQuet <tunyk.93@gmail.com>
2026-07-13 23:43:09 +0700

    chore: add automa-dashboard to ecosystem

--- clone_all.bat
+++ clone_all.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 REM Script to clone or update all Automa Ecosystem repositories on Windows
 
-set "REPOS=automa automa-be automa-cli"
+set "REPOS=automa automa-be automa-cli automa-dashboard"
 set "ORG=tuquet"
 
 echo Cloning or updating Automa Ecosystem repositories...
